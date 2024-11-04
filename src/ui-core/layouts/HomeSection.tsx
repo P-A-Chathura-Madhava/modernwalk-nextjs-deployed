8975859ac4a344a9ba096aaec88d467aa584a2a7
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-11-04 09:45:59 +0530

    docs: Add develop branch

--- src/ui-core/layouts/HomeSection.tsx
+++ src/ui-core/layouts/HomeSection.tsx
@@ -1,6 +1,5 @@
 import SubHeading from "../components/atoms/HomePage/SubHeading";
 import CategoriesRaw from "../components/molecules/HomePage/CategoriesRaw";
-// import ProductRaw from "../components/molecules/HomePage/ProductRaw";
 
 function HomeSection({ ...props }) {
   const { productState } = props;
@@ -8,7 +7,6 @@ function HomeSection({ ...props }) {
   return (
     <section className="container mx-auto mt-2 px-60 font-poppins">
       <SubHeading {...{ title: "Flash Sale" }} />
-      {/* <ProductRaw {...{ productState }} /> */}
       <div className="mt-7"></div>
       <SubHeading {...{ title: "Categories" }} />
       <CategoriesRaw />
