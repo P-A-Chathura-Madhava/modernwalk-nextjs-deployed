08bb3744371bdfda99e94e22eb94efbfa97a0394
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-11-11 11:06:06 +0530

    style: Update Product Detail Page

--- src/ui-core/components/molecules/ProductCard/ProductCard.tsx
+++ src/ui-core/components/molecules/ProductCard/ProductCard.tsx
@@ -1,13 +1,20 @@
 import Link from "next/link";
 import ProductCardImage from "./ProductCardImage";
 import ProductDetail from "./ProductDetail";
+import { useRouter } from "next/navigation";
 
 function ProductCard({ ...props }) {
   const { id, title, image, price, description, category } = props;
+  const router = useRouter();
+
+  const goToProductDetailsHandler = () => {
+    router.push(`mens-products/${props.id}`);
+  }
 
   return (
     <div
       className={`w-[170px] h-[224px] rounded-2xl shadow-black shadow-3xl cursor-pointer hover:shadow-4xl transition-all duration-300 pt-2 font-poppins`}
+      onClick={goToProductDetailsHandler}
     >
       <ProductCardImage {...{ id, title, image, price, description }} />
       <ProductDetail {...{ price, description, category }} />
